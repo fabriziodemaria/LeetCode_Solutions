f9f7a946898e44c580bc03fc5b1b71abd2a03db5
fabriziodemaria <fabrizio.f.demaria@gmail.com>
2015-11-20 23:32:34 +0100

    Bugfix scheduler

--- LeetCode_Daily.py
+++ LeetCode_Daily.py
@@ -4,12 +4,12 @@ import subprocess
 def main():
 	result = subprocess.check_output("ls ~/Developer/LeetCode_Staged/ | head -n 1", shell=True)
 	result2 = str(result.replace(" ", "\ "))[:-1]
-	print "cp -R ~/Developer/LeetCode_Staged/" + result2 + " ."
+	print "( cd ~/Developer/LeetCode_Solutions && cp -R ~/Developer/LeetCode_Staged/" + result2 + " . )"
 	subprocess.check_output("cp -R ~/Developer/LeetCode_Staged/" + result2 + " .", shell=True)
-	subprocess.check_output("git add " + result2, shell=True)
-	subprocess.check_output("git commit -m \"Add " + result + "\"", shell=True)
+	subprocess.check_output("( cd ~/Developer/LeetCode_Solutions && git add " + result2 + " )", shell=True)
+	subprocess.check_output("( cd ~/Developer/LeetCode_Solutions && git commit -m \"Add " + result + "\" )", shell=True)
 	subprocess.check_output("rm -R ~/Developer/LeetCode_Staged/" + result2, shell=True)
-	subprocess.check_output("git push", shell=True)
+	subprocess.check_output("( cd ~/Developer/LeetCode_Solutions && git push )", shell=True)
 
 
 if __name__ == "__main__":
