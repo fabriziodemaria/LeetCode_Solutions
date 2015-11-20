863f6b107943b28dda76a9302cb324e7f03e6ba7
fabriziodemaria <fabrizio.f.demaria@gmail.com>
2015-11-20 23:08:29 +0100

    Add script to schedule commits

---
+++ LeetCode_Daily.py
@@ -0,0 +1,13 @@
+import os
+import subprocess
+
+def main():
+	result = subprocess.check_output("ls ~/Developer/LeetCode_Staged/ | head -n 1", shell=True)
+	result2 = str(result.replace(" ", "\ "))[:-1]
+	print "cp -R ~/Developer/LeetCode_Staged/" + result2 + " ."
+	subprocess.check_output("cp -R ~/Developer/LeetCode_Staged/" + result2 + " .", shell=True)
+	subprocess.check_output("git add " + result2, shell=True)
+	subprocess.check_output("git commit -m \"" + result + "\"", shell=True)
+
+if __name__ == "__main__":
+    main()
