f8eef501409ff2f49356cda0b4d535983193e4e3
fabriziodemaria <fabrizio.f.demaria@gmail.com>
2015-11-20 23:14:03 +0100

    Update schedule script

--- LeetCode_Daily.py
+++ LeetCode_Daily.py
@@ -7,7 +7,10 @@ def main():
 	print "cp -R ~/Developer/LeetCode_Staged/" + result2 + " ."
 	subprocess.check_output("cp -R ~/Developer/LeetCode_Staged/" + result2 + " .", shell=True)
 	subprocess.check_output("git add " + result2, shell=True)
-	subprocess.check_output("git commit -m \"" + result + "\"", shell=True)
+	subprocess.check_output("git commit -m \"Add " + result + "\"", shell=True)
+	subprocess.check_output("rm -R ~/Developer/LeetCode_Staged/" + result2, shell=True)
+	subprocess.check_output("git push", shell=True)
+
 
 if __name__ == "__main__":
     main()
