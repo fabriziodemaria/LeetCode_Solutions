43b0853bc5ab3513f29209f60a3839d12f276afa
fabriziodemaria <fabrizio.f.demaria@gmail.com>
2015-11-21 00:05:24 +0100

    Handle absolute paths and hidden files

--- LeetCode_Daily.py
+++ LeetCode_Daily.py
@@ -2,14 +2,14 @@ import os
 import subprocess
 
 def main():
-	result = subprocess.check_output("ls ~/Developer/LeetCode_Staged/ | head -n 1", shell=True)
+	result = subprocess.check_output("ls /Users/fabriziodemaria/Developer/LeetCode_Staged/ | grep -v '^\.' | head -n 1", shell=True)
 	result2 = str(result.replace(" ", "\ "))[:-1]
-	print "( cd ~/Developer/LeetCode_Solutions && cp -R ~/Developer/LeetCode_Staged/" + result2 + " . )"
-	subprocess.check_output("cp -R ~/Developer/LeetCode_Staged/" + result2 + " .", shell=True)
-	subprocess.check_output("( cd ~/Developer/LeetCode_Solutions && git add " + result2 + " )", shell=True)
-	subprocess.check_output("( cd ~/Developer/LeetCode_Solutions && git commit -m \"Add " + result + "\" )", shell=True)
-	subprocess.check_output("rm -R ~/Developer/LeetCode_Staged/" + result2, shell=True)
-	subprocess.check_output("( cd ~/Developer/LeetCode_Solutions && git push )", shell=True)
+	print "( cd /Users/fabriziodemaria/Developer/LeetCode_Solutions && cp -R /Users/fabriziodemaria/Developer/LeetCode_Staged/" + result2 + " . )"
+	subprocess.check_output("cp -R /Users/fabriziodemaria/Developer/LeetCode_Staged/" + result2 + " .", shell=True)
+	subprocess.check_output("( cd /Users/fabriziodemaria/Developer/LeetCode_Solutions && git add " + result2 + " )", shell=True)
+	subprocess.check_output("( cd /Users/fabriziodemaria/Developer/LeetCode_Solutions && git commit -m \"Add " + result + "\" )", shell=True)
+	subprocess.check_output("rm -R /Users/fabriziodemaria/Developer/LeetCode_Staged/" + result2, shell=True)
+	subprocess.check_output("( cd /Users/fabriziodemaria/Developer/LeetCode_Solutions && git push )", shell=True)
 
 
 if __name__ == "__main__":
