1f1a972b36b8f81c0f56554638b113fe6f5b8f2e
fabriziodemaria <fabrizio.f.demaria@gmail.com>
2015-12-11 17:55:07 +0100

    Add H-Index

---
+++ H-Index/main.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int hIndex(int[] citations) {
+        Arrays.sort(citations);
+        int j = 1;
+        for (int i = citations.length - 1; i >= 0; i--) {
+            if (citations[i] < j) return j-1; 
+            j++;
+        }
+        return j-1;
+    }
+}
