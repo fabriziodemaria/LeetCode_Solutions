fecb2e41ca441ac65595451aa220ab655aa47219
fabriziodemaria <fabrizio.f.demaria@gmail.com>
2016-02-14 00:00:28 +0100

    Add Number of 1 Bits

---
+++ Number of 1 Bits/main.java
@@ -0,0 +1,11 @@
+class Solution {
+    // you need to treat n as an unsigned value
+    public int hammingWeight(int n) {
+        int counter = 0;
+                while (n != 0) {
+            if ((n&1) == 1) counter++;
+            n >>>= 1;
+        }
+                return counter;
+    }
+}
